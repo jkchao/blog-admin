43453c16a4185f745f62a1212811b375a5f74bc1
jkchao <jkchaom@gmail.com>
2018-01-26 14:51:05 +0800

    update heros pages

--- src/pages/heros/index.vue
+++ src/pages/heros/index.vue
@@ -83,7 +83,7 @@
         </el-table-column>
         <el-table-column
           label="操作"
-          width="220"
+          width="280"
           label-class-name="head"
           fixed="right">
           <template slot-scope="scope">
