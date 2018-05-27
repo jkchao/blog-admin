0ac62c0a0fc77fa08298e261ac3e01071086fd71
jkchao <jkchaom@gmail.com>
2018-05-27 13:21:15 +0800

    nooe

--- bin/index.js
+++ bin/index.js
@@ -4,35 +4,38 @@ const path = require('path')
 const enPath = path.resolve(__dirname, '../src/ajax')
 const outPath = path.resolve(__dirname, '../src/api/test')
 
+const template = template => {
+  return `// ${template.des}\n` +
+          `export function ${template.name} (params?: any)` +
+          `: Promise<Ajax.AjaxResponse> {\n` +
+          `  return ax.${template.method}('${template.url}', ` +
+          `${template.method === 'get' ? '{ params }' : 'params'})\n` +
+          `           .then(res => res.data)\n` +
+          `           .catch(error => console.error)\n` +
+          `}\n\n`
+}
+
+// 目录不存在时，创建
+if (!fs.existsSync(outPath)) {
+  fs.mkdirSync(outPath)
+}
+
 const files = fs.readdirSync(enPath)
 
 files.forEach(file => {
   const data = fs.readFileSync(`${enPath}/${file}`)
 
-  if (!JSON.parse(data)) {
-    console.log('error')
-    return
-  }
-
   const fileName = file.split('.')[0]
-  const content = JSON.parse(data)
 
-  let result = "import Ax from '../axios'\n\n"
+  let result = ''
 
-  content.forEach(item => {
-    result += `// ${item.des}\n` +
-              `export function ${item.name} (params?: any)` +
-              `: Promise<Ajax.AjaxResponse> {\n` +
-              `  return Ax.${item.method}('${item.url}', ` +
-              `${item.method === 'get' ? '{ params }' : '{ ...params }'})\n` +
-              `           .then(res => res.data)\n` +
-              `           .catch(error => console.error)\n` +
-              `}\n\n`
-  })
+  if (data.length) {
+    result = "import ax from '../axios'\n\n"
+    const content = JSON.parse(data)
 
-  // 目录不存在时，创建
-  if (!fs.existsSync(outPath)) {
-    fs.mkdirSync(outPath)
+    content.forEach(item => {
+      result += template(item)
+    })
   }
 
   fs.writeFileSync(`${outPath}/${fileName}.ts`, result)
